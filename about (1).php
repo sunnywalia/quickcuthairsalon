2e2dbfc51f020aa37bf4043d6d1024a47e5b2238
Bhawanikatoch07 <47265538+Bhawanikatoch07@users.noreply.github.com>
2019-02-02 19:06:29 +1000

    Add files via upload
    i updated code

---
+++ about (1).php
@@ -0,0 +1,190 @@
+<?php include('confige.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style type="text/css">
+    
+
+.more a {
+    display: inline-block;
+    font-size: 1.5em;
+    color: #fff;
+    background: #ffcc54;
+    padding: .6em 3em;
+    margin-top: .8em;
+    margin-bottom: 5%;
+    border-radius: 4px;
+    -webkit-transition: .5s all;
+    -moz-transition: .5s all;
+    transition: .5s all;
+}
+.more a:hover{
+
+
+  color: black;
+  background: white;
+  /*border: 3px solid white;*/
+  box-shadow: 3px 5px 5px 5px black;
+  border-radius: 15px;
+  text-transform: capitalize;
+    text-align: center;
+    font-family: cursive;
+    font-weight: bold;
+    box-sizing: border-box;
+    
+}
+.w3l_header {
+    font-size: 3em;
+    color: #262c38;
+    letter-spacing: 1px;
+    position: relative;
+    font-weight: 600;
+    text-align: center;
+}
+.w3l_header span {
+    color: #545151;
+    font-weight: 200;
+}
+.w3l_header:after {
+    border-top: 2px solid #ffcc54;
+    display: block;
+    width: 300px;
+    content: "";
+    margin: 8px auto 0;
+}
+
+  </style>
+
+
+</head>
+<?php include('header.php'); ?>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+	
+
+	<div class="clear"></div>
+	<div class="service">
+			<h2>QUICK CUT STYLE UNISEX SALON</h2>
+			<p style="width:80%; margin-left: 8%;">
+				QUICK CUT STYLE UNISEX SALON The salon offers a full range of cutting edge salon services and products. They provide scissor, clipper/razor services for men and women, children's services, shampoo/blow-dry, thermal finishing, coloring with "Schwarzkopf", perming waxing and more..
+			</p>
+			<img src="images/about.jpg">
+			<div class="clear"></div>
+
+      <h2>OUR VALUES</h2>
+      <h3 class="w3l_header">Our <span>Mission</span></h3>
+
+      <p style="width:80%; margin-left: 8%;">
+        MISSION:OUR MISSION STARTS WITH BECOMING THE PREMIER SALON IN DUBAI& EMPLOYER OF CHOICE IN THE BEAUTY SERVICES INDUSTRY, TOWARDS ENSURING THAT OUR TEAM IS ALWAYS AT THE EXPECTATIONS OF OUR VALUABLE CLIENTS PROVIDING THEM SUPERIOR SERVICES WITH THE HIGHEST BEST QUALITY BEAUTY PRODUCTS. WE PROUDLY ARE USING THE BEST WORLD-WIDE BRANDS.
+      </p>
+      <h3 class="w3l_header">Our <span>Vision</span></h3>
+
+      <p style="width:80%; margin-left: 8%;">
+          VISION: To become the premier chain of salons in austelia, then across UAE,and employer of choice in the beauty services.To ensure that our staff are always at the leading edge of the beauty industry. To provide our guests with superior services through our highly qualified staff, and the highest quality beauty products.
+      </p>
+			<h2>Our Customer With Their New Style</h2>
+	
+		</div>
+
+
+
+<div class="container-fluid text-center bg-grey">
+
+
+		<div class="row text-center">
+              <?php
+
+            $result =mysql_query("select * from content limit 3");
+            while($row=mysql_fetch_array($result)){
+
+          ?>
+              <div class="col-sm-4"style="margin-left: 3%; margin-bottom:5%; box-sizing: border-box;">
+                <div class="thumbnail">
+                  <img src="images/<?php echo $row['image']; ?>" alt="GRP pipes" width="400" height="300">
+                  <p><strong><?php echo $row['job']; ?></strong></p>
+                  <p><?php echo $row['description']; ?></p>
+                  <div class="more">
+                      <a href="book.php">Book Now</a>        
+                    </div>
+                </div>
+              </div>
+
+              <?php 
+                }
+              ?>
+    </div>
+
+</div>
+<?php include 'footer.php'; ?>
+
+</body>
+
+</html>
+<!-- <div class="col-sm-4">
+      <div class="thumbnail">
+        <img <s></s>rc="images/2.jpg" alt="Mouldtrusion products" width="400" height="300">
+        <p><strong>Straight Hair products</strong></p>
+        <p> Yes, we provide material for  Straight Hair</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/3.jpg"  alt="Pultrusion optical fibre cable" width="400" height="300">
+        <p><strong>Haircut</strong></p>
+        <p>New Hair Style</p>
+      </div>
+    </div>
+  </div><br>
+  <div class="row text-center ">
+    <div class="col-sm-4" style="margin-left: 5%">
+      <div class="thumbnail">
+        <img src="images/5.jpg" alt="RTM" width="400" height="300">
+        <p><strong>Hair Color</strong></p>
+        <p>Yes, we provide material for  Hair Color</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/6.jpg" alt="SMC Water Tank" width="400" height="300">
+        <p><strong>Trim Hair And Style</strong></p>
+        <p> Trim hair and style</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/7.jpg" alt="Wood coating" width="400" height="300">
+        <p><strong>Haircut</strong></p>
+        <p>New Hair Style and Shave </p>
+      </div>
+    </div>
+  </div><br>
+  <div class="row text-center ">
+    <div class="col-sm-4" style="margin-left: 5%">
+      <div class="thumbnail">
+        <img src="images/8.jpg" alt="Automotive refinish" width="400" height="300">
+        <p><strong> back and side HairCut </strong></p>
+        <p>New Back And Side Style</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/9.jpg" alt="Can coating" width="400" height="300">
+        <p><strong>shave</strong></p>
+        <p> New Shave Style</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="images/5.jpg" alt="Can coating" width="400" height="300">
+        <p><strong>Simple Hair Cut  Touch up</strong></p>
+        <p> Hair Cut  Touch up</p>
+      </div>
+    </div> -->
--- index.php
+++ index.php
@@ -1,201 +1,189 @@
-<?php include('confige.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <style type="text/css">
-  
-@media only screen and (min-width: 0px) and (max-width: 450px)  {
-.slider1{
-
-      background-repeat: no-repeat; 
-      width: 100%;
-      height: 180px;
-      animation: slide 30s infinite;
-      background-size: contain;
-    margin-top:16%;  
-}	
-}
-
-    .service h2 {
-    display: block;
-    color: #00a4e4;
-    font-size: 2.5em;
-    position: inherit;
-    padding-bottom: .5em;
-    text-align: center;
-    margin-top: 1%;
-    text-shadow: 2px 2px 5px black;
-}
-
-.more a {
-    display: inline-block;
-    font-size: 1.5em;
-    color: #fff;
-    background: #ffcc54;
-    padding: .6em 3em;
-    margin-top: .8em;
-    margin-bottom: 5%;
-    border-radius: 4px;
-    -webkit-transition: .5s all;
-    -moz-transition: .5s all;
-    transition: .5s all;
-}
-.more a:hover{
-
-
-  color: black;
-  background: white;
-  /*border: 3px solid white;*/
-  box-shadow: 3px 5px 5px 5px black;
-  border-radius: 15px;
-  text-transform: capitalize;
-    text-align: center;
-    font-family: cursive;
-    font-weight: bold;
-    box-sizing: border-box;
-    
-}
-
-
-
-#slider12{
-
-  display: block;
-  width: 100%;
-  background-color: transparent;
-  position: relative;
-  height: 500px;
-}
-
-  </style>
-
-
+	<title>
+		Wsdesign E-business Technologies
+	</title>
 </head>
+<style type="text/css">
+	body .homepage{
+		
+		display: inline-block;
+		width: 100%
+		height:2500px;
+		position: initial;
+		height: auto;
+		border-radius: 15px;
+		border:1px solid black;
+		box-sizing: border-box;
+
+
+	}
+	.homepage .section{
+
+		margin: 3% 5%;
+		box-sizing: border-box;
+
+	}
+	.homepage .section .box1{
+		float: left;
+		width: 30%;
+		margin-right: 3%;
+		background-color: black;
+		border-radius: 15px;
+		height: 750px;
+		display: inline-block;
+		margin-top: 3%;
+		margin-bottom: 5%;
+		box-sizing: border-box;
+
+	}
+.clearfix{
+
+	clear: both;
+
+
+}
+.homepage .section .box1 h4{
+
+	    font-size: 18px;
+	    color: #ea1d5d;
+	    line-height: 33px;
+    	padding: 3%;
+    	text-align: center;
+    text-transform: uppercase;
+}
+
+.homepage .section .box1 h4:after {
+
+    border-top: 3px solid white;
+
+    display: block;
+    width: 300px;
+    content: "";
+    margin: 8px auto 0;
+}
+.homepage .section .box1 h4:before {
+
+    border-top: 3px solid white;
+
+    display: block;
+    width: 300px;
+    content: "";
+    margin: 8px auto 0;
+}
+.homepage .section .box1 p{
+
+		font-size: 12px;
+	    color: white;
+	    line-height: 33px;
+    	padding: 3%;
+    	text-align: center;
+    	text-transform: uppercase;
+}
+.homepage .section .box1 p:after {
+
+    border-top: 3px solid white;
+
+    display: block;
+    width: 300px;
+    content: "";
+    margin: 8px auto 0;
+}
+.homepage .section .box2{
+float: left;
+		width: 30%;
+		margin-right: 3%;
+		background-color: white;
+		border-radius: 15px;
+		height: 350px;
+		display: inline-block;
+		margin-top: 3%;
+		margin-bottom: 5%;
+		box-sizing: border-box;
+		border:1px solid black;
+
+	}
+.homepage .section .box2 img{
+	border-radius: 15px;
+	width: 90%;
+	margin:1% 5%;
+	height: 250px; 
+}
+
+
+</style>
 <?php include('header.php'); ?>
-
- 
-  <?php include('header1.php'); ?>
-
-<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
-   <div class="clear"></div>
-	<div class="clearfix"></div>
-
-  <div class="service">
-   
+<body>
+<div class="clearfix"></div>
+<div class="homepage">
+	<div class="section">
+	<div class="box1">
+		<h4>WEB DESIGN USING HTML AND OTHER TOOLS</h4>
+                <p>As a professional web designing service provider, we offer affordable browsing friendly solutions in addition to creating a new pattern. We include technology with creativity to deliver an elegant and professional way of creating an online identity for your product needs. In addition, it is quiet important to tackle your web appearance which should not only be informative, it should also have a pleasing look ensuring the ability to attract more and more visitors. This is the only critical and crucial way for the success of your firm. Here at Opal Infotech, we have a highly expertise team of web developers, who never miss even minute niche while bring forth maximum information about the purpose behind your business appearance in the internet.</p>
+	</div>
 
 
-      <!--<img src="images/about.jpg">-->
+	<div class="box1">
+                <h4>Responsive Web Design / Mobile Website</h4>
+                <p>It is an approach where a site is adjusted for optimal viewing on a range of platforms and devices of different sizes. Such devices include mobile phones, net-books, notebooks, laptops, smartphones, desktops and tablets, which have different monitor sizes and different viewing environments. The intention of creating such an online link is to optimize viewing experience, as well as to re-size the link for ease of scrolling and panning. It can also help businesses present their page content in different platforms and browsers that are compatible with different devices.In this type of creation, the internet pages are coded using the cascading style sheets and with use of media query          In this type of creation, the internet pages are coded using the CSS.</p>
+		
+	</div>
 
-			<h2>Welcome To Our Salon</h2>
-			<p style="width:80%; margin-left: 10%; text-transform: lowercase; font-weight: 500;">
-				QUICK CUT STYLE UNISEX SALON IS A ONE PLACE FOR ALL YOUR BEAUTY NEEDS. WITH DIFFERENT BEAUTY SERVICES; BEST STANDARDS IN CLEANLINESS AND SANITIZATION, BEST SERVICE TECHNIQUES; QUICK CUT STYLE UNISEX SALON COULD DRAW AN EDGE IN THE BEAUTY MARKET. OUR SERVICES INCLUDE BUT NOT LIMITED TO NAIL THERAPY, MANICURE & PEDICURE, HAIR TREATMENT, SCULPTURE CUT, SHAPING AND STYLING, HAIR COLORING, SKIN TREATMENT AND THERAPY, ALL PERFORMED BY AN EXPERT AND FRIENDLY TEAMS.
-			</p>
-			<div class="clear"></div>
 
-			<h2>Our Customer With Their New Style</h2>
+	<div class="box1">
+                <h4>Custom PHP Web Design & Development</h4>
+                <p>PHP is perhaps the most effective and powerful programming language used to develop dynamic sites and applications. Over the years PHP web design and development has been continuously updated which has made this programming language highly interactive and allows for creating applications and layouts which have been used at a mass scale in today’s technological world. Opal Infotech has extensive knowledge and experience of providing web developing services using this popular programming language. Our experienced professionals are dedicated to PHP programming and expertise in different kinds of services complementing the design process including content management, promotion of websites online and many other services.</p>
+
+		
+	</div>
+
+
+<div class="box1">
+	<h4>CodeIgniter</h4>
+                <p>CodeIgniter is an Application Development Framework - a toolkit - for people who build web sites using PHP. Its goal is to enable you to develop projects much faster than you could if you were writing code from scratch, by providing a rich set of libraries for commonly needed tasks, as well as a simple interface and logical structure to access these libraries. CodeIgniter lets you creatively focus on your project by minimizing the amount of code needed for a given task.CodeIgniter is right for you if:1)You want a framework with a small footprint.2)You need exceptional performance.3)You need broad compatibility with standard hosting accounts that run a variety of PHP versions and configurations.4)You want a framework that requires nearly zero configuration.5)You want a framework that does not require you to use the command line.
+</p>
 	
-		</div>
-
-
-
-<div class="container-fluid text-center bg-grey">
-
-
-		<div class="row text-center">
-              <?php
-
-            $result =mysql_query("select * from content");
-            while($row=mysql_fetch_array($result)){
-
-          ?>
-              <div class="col-sm-4"style="margin-left: 3%; margin-bottom:5%; box-sizing: border-box;">
-                <div class="thumbnail">
-                  <img src="images/<?php echo $row['image']; ?>" alt="GRP pipes" width="400" height="300">
-                  <p><strong><?php echo $row['job']; ?></strong></p>
-                  <p><?php echo $row['description']; ?></p>
-                  <div class="more">
-                      <a href="book.php">Book Now</a>        
-                    </div>
+</div>
+<div class="box1">
+	<h4>WordPress Web Design</h4>
+                <p>
+                  WordPress is an open source content management system and a blogging tool powered by PHP and MySQL. Currently WordPress is known as the most easy to use content management systems available. Using this CMS, one can develop business small or large web sites and blogs. As It is an open source CMS, it can be downloaded from the internet free of cost. After downloading WP, one can immediately start posting and publishing the content. It has number of in built features which provides facility to create and maintain the internet pages in an effective manner. It plays an important role in creating functional and attractive internet page and which can give an online presence to your business quickly.With the advancement in the system, it is now not only used to create blogs, but also for creating custom pages. </p>
+</div>
+<div class="box1">
+	<h4>Android Marketplace</h4>
+                <p>Android app market is growing on large scale, as Android seems to be the first preference of majority Smartphone users Nowadays. More than 80% mobile segment has been captured by Android and 800 million users use Android apps as of now. So there is not at all, over to say that Android is the leading mobile platform that grows exponentially day by day. Hence, it is utmost crucial to develop android application to focus, expand and get success in your business.Platform specific application will be useful to that mobile Operating system only and not for others. Yet, several businesses need wide spectrum application to be catered on all mobile platforms including IPhones, Android gadgets, Windows phone, Blackberry etc. This can be a challenging situation for an app developer to indentify and define the app as per the present and future business needs and scalability.</p>
+</div>	
+        <h3 class="title text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">View Our Work </h3>
+<div class="box2">
+              <img src="images/ww2.jpg" class="img-fluid" alt="">
+               <div class="blog-txt-info">
+                <h4 class="mt-2 text-center"><a href="#" data-toggle="modal" data-target="#exampleModalLive" data-blast="color">ABC CONSULTANCY.COM </a></h4>
                 </div>
-              </div>
+	
+</div>
+<div class="box2">
+              <img src="images/ww4.jpg" class="img-fluid" alt="">
+              <div class="blog-txt-info">
+                <h4 class="mt-3 text-center"><a href="#" data-toggle="modal" data-target="#exampleModalLive" data-blast="color">CONSULTANCY.COM </a></h4>
+            </div>
+              
+	
+</div>
 
-              <?php 
-                }
-              ?>
-    </div>
+<div class="box2">
+              <img src="images/ww1.jpg" class="img-fluid" alt="">
+              <div class="blog-txt-info">
+                <h4 class="mt-3 text-center"><a href="#" data-toggle="modal" data-target="#exampleModalLive" data-blast="color">WE PROFESSIONAL CONSULTANCY </a></h4>
+            </div>
+	
+</div>
 
 </div>
-<?php include 'footer.php'; ?>
+
+</div>
+<div class="clearfix"></div>
 
 </body>
-
 </html>
-<!-- <div class="col-sm-4">
-      <div class="thumbnail">
-        <img <s></s>rc="images/2.jpg" alt="Mouldtrusion products" width="400" height="300">
-        <p><strong>Straight Hair products</strong></p>
-        <p> Yes, we provide material for  Straight Hair</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="images/3.jpg"  alt="Pultrusion optical fibre cable" width="400" height="300">
-        <p><strong>Haircut</strong></p>
-        <p>New Hair Style</p>
-      </div>
-    </div>
-  </div><br>
-  <div class="row text-center ">
-    <div class="col-sm-4" style="margin-left: 5%">
-      <div class="thumbnail">
-        <img src="images/5.jpg" alt="RTM" width="400" height="300">
-        <p><strong>Hair Color</strong></p>
-        <p>Yes, we provide material for  Hair Color</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="images/6.jpg" alt="SMC Water Tank" width="400" height="300">
-        <p><strong>Trim Hair And Style</strong></p>
-        <p> Trim hair and style</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="images/7.jpg" alt="Wood coating" width="400" height="300">
-        <p><strong>Haircut</strong></p>
-        <p>New Hair Style and Shave </p>
-      </div>
-    </div>
-  </div><br>
-  <div class="row text-center ">
-    <div class="col-sm-4" style="margin-left: 5%">
-      <div class="thumbnail">
-        <img src="images/8.jpg" alt="Automotive refinish" width="400" height="300">
-        <p><strong> back and side HairCut </strong></p>
-        <p>New Back And Side Style</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="images/9.jpg" alt="Can coating" width="400" height="300">
-        <p><strong>shave</strong></p>
-        <p> New Shave Style</p>
-      </div>
-    </div>
-    <div class="col-sm-4">
-      <div class="thumbnail">
-        <img src="images/5.jpg" alt="Can coating" width="400" height="300">
-        <p><strong>Simple Hair Cut  Touch up</strong></p>
-        <p> Hair Cut  Touch up</p>
-      </div>
-    </div> -->
+<?php include('footer.php'); ?>
